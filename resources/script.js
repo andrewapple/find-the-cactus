a44f2c1fef0d79949d8954837b0e4476dc91d4a4
Andrew Apple <andrewdapple@gmail.com>
2022-11-28 22:11:41 -0500

    cleaned up code

--- resources/script.js
+++ resources/script.js
@@ -6,13 +6,4 @@ function choice(card1, card2) {
 
     choice1.style.opacity = 0;
     choice2.style.opacity = 1;
-
-    // if(album1.style.visibility === "hidden" && album2.style.visibility === "hidden"){
-    //     album1.style.visibility = "visible";
-    // }else if(album1.style.visibility === "visible" || album2.style.visibility === "visible"){
-    //     album2.style.visibility = "hidden";
-    //     album1.style.visibility = "visible";
-    // }else {
-    //     album1.style.visibility = "visible";
-    // }
 }
