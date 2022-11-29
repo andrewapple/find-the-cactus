f20e7b7720aeb7ccdc9dd7882104b2b056db9e5e
Andrew Apple <andrewdapple@gmail.com>
2022-11-28 22:06:31 -0500

    initial commit

---
+++ resources/script.js
@@ -0,0 +1,18 @@
+
+
+function choice(card1, card2) {
+    var choice1 = document.getElementById(card1);
+    var choice2 = document.getElementById(card2);
+
+    choice1.style.opacity = 0;
+    choice2.style.opacity = 1;
+
+    // if(album1.style.visibility === "hidden" && album2.style.visibility === "hidden"){
+    //     album1.style.visibility = "visible";
+    // }else if(album1.style.visibility === "visible" || album2.style.visibility === "visible"){
+    //     album2.style.visibility = "hidden";
+    //     album1.style.visibility = "visible";
+    // }else {
+    //     album1.style.visibility = "visible";
+    // }
+}
